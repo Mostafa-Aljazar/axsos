625b0175fdde410efe90e5ee1bf2d2a974ae93e8
Mostafa-Aljazar <mostafaibrahim20032020@gmail.com>
2026-03-19 11:34:32 +0200

    chore: solve Algorithms_And_Data_Structure/Practice_I

---
+++ JS/Algorithms_And_Data_Structure/Practice_I/index.js
@@ -0,0 +1,53 @@
+// favoriteAnimal
+console.log("🚀 ~ favoriteAnimal:")
+
+let favoriteAnimal = "Cat";
+console.log(favoriteAnimal);
+
+// sum of a and b
+console.log("🚀 ~ sum of a and b:")
+
+let a = 5;
+let b = 7;
+console.log(a + b);
+
+// age message
+console.log("🚀 ~ age message:")
+
+let age = 25;
+console.log("I am " + age + " years old.");
+
+// change country
+console.log("🚀 ~ change country:")
+
+let country = "Japan";
+country = "Italy";
+console.log(country);
+
+// const pi
+console.log("🚀 ~ const pi:")
+
+const pi = 3.14;
+console.log(pi);
+
+// favoriteColor
+console.log("🚀 ~ favoriteColor:")
+
+let favoriteColor = "Blue";
+console.log(favoriteColor);
+
+// sum x and y
+console.log("🚀 ~ sum x and y:")
+
+let x = 5;
+let y = 10;
+console.log(x + y);
+
+// isSunny message
+console.log("🚀 ~ isSunny message:")
+
+let isSunny = true;
+
+if (isSunny) {
+  console.log("It is sunny today.");
+}
